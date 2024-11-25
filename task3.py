16c5ccc247eb333153560ab6e1cb9e953168b7d5
Nader2005 <Nader2005@example.com>
2024-11-25 12:52:46 +0200

    'some changes in my code'

--- task3.py
+++ task3.py
@@ -1,6 +1,7 @@
 import random
 print("Welcome to the Word Scramble Game!")
-word = "Nader"
+word_list = ["Nader" , "Elsayed" , "Hassan" , "Soliman"]
+word = random.choice(word_list)
 word_list = list(word)
 random.shuffle(word_list)
 scramble_word = ''.join(word_list)
