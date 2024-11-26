7334732402357674388899894d7bdcbec7b74251
Nader2005 <Nader2005@example.com>
2024-11-26 17:07:42 +0200

    'Adding some comments to my code to explain what mean'

--- task3.py
+++ task3.py
@@ -2,20 +2,20 @@ import random
 print("Welcome to the Word Scramble Game!")
 word_list = ["Nader" , "Elsayed" , "Hassan" , "Soliman"]
 word = random.choice(word_list)
-chosen_word = list(word)
+chosen_word = list(word) # Nader-> ['N' , 'a' , 'd' , 'e' , 'r']
 random.shuffle(chosen_word)
 scramble_word = ''.join(chosen_word)
 print(f"Try to guess the original word from the scrambled word: {scramble_word}")
 x = 5
-print(f"You have {x} attempts")
+print(f"You have {x} attempts.")
 while x > 0:
-  z = input("Enter your guess:")
-  if(z == word):
-    print("Congratulations! You guessed the correct word!")
-    break
-  else:
-    x -= 1
-    if(x > 0):
-      print (f"Incorrect! Try again. You have {x} attempts left.")
+    z = input("Enter your guess: ")
+    if(z == word):
+      print("Congratulations! You guessed the correct word!")
+      break
     else:
-      print(f"You’re out of attempts! The correct word was: {word}")
+        x -= 1 # x = x-1
+        if(x > 0):
+            print(f"Incorrect! Try again. You have {x} attempts left.")
+        else:
+            print(f"You’re out of attempts! The correct word was: {word}")
